32c7f19c508249925a95464fd2263e4a908df052
Bart Coddens <bart.coddens@gmail.com>
2014-02-06 17:13:11 +0100

    Added pull script

--- pull.sh
+++ pull.sh
@@ -1,1 +1,6 @@
+cd /data/illumos/master/illumos-gate
 git pull upstream master -v
+cd /data/illumos/dev/illumos-gate
+git pull origin master -v
+cd /data/illumos/deploy/illumos-gate
+git pull origin master -v
